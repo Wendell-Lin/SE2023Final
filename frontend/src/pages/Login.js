681d840ad66ec4dd43cefec81e71a907ad4c704d
Techuang <chuangellow@gmail.com>
2023-11-21 22:24:26 +0800

    Init the Login and Register Page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
 
-const Login = ({login}) => {
-    return <button onClick={login}>login</button>
+function Login() {
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <h1>Welcome back!</h1>
+        <p>Enter your Credentials to access your account</p>
+        <form onSubmit={handleLogin}>
+          <label htmlFor="email">Email address</label>
+          <input type="email" id="email" placeholder="Enter your email" required />
+          
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Password" required />
+          
+          <div className="remember">
+            <input type="checkbox" id="terms" />
+            <label htmlFor="terms" className="custom-checkbox"></label>
+            <label htmlFor="terms">Remember for 30 days</label>
+          </div>
+          
+          <button type="submit">Login</button>
+        </form>
+        <div className="login-footer">
+          Don’t have an account? <a href="/register">Sign Up</a>
+        </div>
+      </div>
+      <div className="login-image">
+        <img src="/images/log-decorative.png" alt="Decorative" />
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './Register.css';
 
-const Register = ({navHome}) => {
-    return <button onClick={navHome}>Register</button>
+function Register() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="register-container">
+      <div className="register-form">
+        <h1>Get Started Now</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" placeholder="Enter your name" required />
+          
+          <label htmlFor="email">Email address</label>
+          <input type="email" id="email" placeholder="Enter your email" required />
+          
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Password" required />
+          
+          <div className="terms-and-policy">
+            <input type="checkbox" id="terms" required />
+            <label htmlFor="terms" className="custom-checkbox"></label>
+            <label htmlFor="terms">I agree to the terms & policy</label>
+          </div>
+          <button type="submit">Sign up</button>
+        </form>
+        <div className="signin-redirect">
+          Have an account? <a href="/login">Sign In</a>
+        </div>
+      </div>
+      <div className="register-image">
+        <img src="/images/log-decorative.png" alt="Decorative" />
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;

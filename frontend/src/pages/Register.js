22275e90d084415cc5ad4436e2ff1c857e002629
Techuang <chuangellow@gmail.com>
2023-11-22 15:08:15 +0800

    Add the feature to show the agreement & policy

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -6,12 +6,17 @@ function Register() {
   const [email, setEmail] = useState('');
   const [emailValid, setEmailValid] = useState(true);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
     setEmailValid(
       /^[\w-\.]+@ntu\.edu\.tw$/i.test(event.target.value)
     );
+  };
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
   const handleSubmit = (event) => {
@@ -62,8 +67,23 @@ function Register() {
             checked={isTermsChecked}
           />
           <label htmlFor="terms" className="custom-checkbox"></label>
-          <label htmlFor="terms">I agree to the terms & policy</label>
+          <label htmlFor="terms">I agree to the <span onClick={toggleModal} className="terms-link">terms & policy</span></label>
           </div>
+          {isModalOpen && (
+          <div className="modal-backdrop">
+            <div className="modal">
+              <h2>免責條款</h2>
+              <p>歡迎來到 Feast Forward，我們的目標是通過分享剩餘食物來減少食物浪費。在使用我們的網站及其服務之前，請仔細閱讀以下免責條款：</p>
+              <ul>
+                <li>Feast Forward 提供一個平台，用於個人和組織之間贈與剩餘食物。請注意，我們不製造、儲存或直接處理這些食品。因此，我們無法保證贈與的食品符合特定的品質標準或衛生要求。我們強烈建議贈與者和接收者遵守當地食品安全與衛生法規。</li>
+                <li>使用我們的網站即表示您同意遵守我們的使用條款，包括但不限於遵守所有適用的法律規範。</li>
+                <li>對於通過我們的網站贈與或接收的食品所可能引起的任何健康問題或其他相關問題，Feast Forward 管理員概不負責。使用者應自行承擔所有風險。</li>
+              </ul>
+              <p>本免責條款可能會不定期更新。我們鼓勵您定期查看本條款，以保持對任何變更的了解。</p>
+              <button onClick={toggleModal}>Close</button>
+            </div>
+          </div>
+        )}
           <button type="submit">Sign up</button>
         </form>
         <div className="signin-redirect">

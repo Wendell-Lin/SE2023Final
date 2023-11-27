96e389bb8b7653b7b7510b1e5d921cd11e548e7f
Livingpool <52132459+Livingpool@users.noreply.github.com>
2023-11-27 12:11:05 +0800

    ADD: add Item & Exception Module, createItem API

--- backend/src/main/java/com/feastforward/controller/TestController.java
+++ backend/src/main/java/com/feastforward/controller/TestController.java
@@ -1,10 +1,7 @@
 package com.feastforward.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -19,6 +16,12 @@ public class TestController {
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public String userAccess() {
         return "User Content.";
+    }
+
+    @PostMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userPostAccess() {
+        return "User Post Content.";
     }
 
     @GetMapping("/mod")

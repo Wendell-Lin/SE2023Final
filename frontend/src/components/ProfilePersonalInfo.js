cbd017c0a8cb96e911818a5cc6d8b6ea30afc3a1
yammy0623 <83154615+yammy0623@users.noreply.github.com>
2023-11-28 03:51:31 +0800

    fix profile form problem

--- frontend/src/components/ProfilePersonalInfo.js
+++ frontend/src/components/ProfilePersonalInfo.js
@@ -1,17 +1,56 @@
 import React, {useState} from "react";
 import './ProfilePersonalInfo.css';
-import { useNavigate } from 'react-router-dom';
 
+// photo
 function PersonalInfo({userInfo}) {
 
-  const [editInfo, setEditInfo] = useState(false)  
-  const navigate = useNavigate() 
-
+  // update Form Data
+  const [userInfoData, setUserInfo] = useState(userInfo);
+  const [formData, setFormData] = useState(userInfo);
+  const [succeedMsg, setSucceedMsg] = useState('');
   const [isSentEmail, setIsSentEmail] = useState(userInfo.notification);
+  
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  // Submit
+  const handleUpload = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setSucceedMsg("Successfully Edit User Info");
+    setUserInfo(formData)
+    setFormData({
+      name: "",
+      email: userInfo.email, // unchanged
+      notification: userInfoData.notification,
+      userImg: userInfoData.userImg,
+      oldpassword:"",
+      newpassword:"",
+      newpassword2:""
+    })
+  };
+
+  // click email
+  const [editInfo, setEditInfo] = useState(false)  
+  
   function sentEditInfo() {
     setEditInfo((prevEditInfo) => !prevEditInfo);
   }
+  const handleCheckboxChange = (e) => {
+    setIsSentEmail(e.target.checked);
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      notification: !isSentEmail,
+    }));
+  };
 
+  // 有問題
+  // Image
   const [selectedFile, setSelectedFile] = useState(null);
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -22,35 +61,17 @@ function PersonalInfo({userInfo}) {
     document.getElementById('fileInput').click();
   };
 
-
-  // -----pop up
-  // const [userInfo, setUserInfo] = useState();
-  const [formData, setFormData] = useState(userInfo);
-  const [succeedMsg, setSucceedMsg] = useState('');
-  const handleCheckboxChange = (e) => {
-    setIsSentEmail(e.target.checked);
-  };
-
-  // const handleInputChange = (e) => {
-  //   setFormData({
-  //     ...formData,
-  //     [e.target.id]: e.target.value,
-  //   });
-  // };
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevFormData => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
-  const handleUpload = (event) => {
-    event.preventDefault();
-    console.log(userInfo);
-    // 模拟上传成功
-    setSucceedMsg("Successfully Edit User Info");
-    // 在这里执行将数据发送到服务器的逻辑
-  };
+  // 比較密碼
+  // const handlePasswordChange = () =>{
+  //   const msg = ""
+  //   if (formData.newpassword === formData.newpassword2){
+  //     msg ="correct";
+  //   }
+  //   else{
+  //     msg ="Please confirm your new password";
+  //   }
+      
+  // }
   
   return (
     <>
@@ -67,6 +88,7 @@ function PersonalInfo({userInfo}) {
 
 
       <div className="profile">
+        <form onSubmit={handleUpload}>
         <div className="edit-profile">Edit Personal Information</div>
         <div className="profile-picNname">
           <div className="picture_frame">
@@ -78,76 +100,82 @@ function PersonalInfo({userInfo}) {
               onChange={handleFileChange}
             />
             <img
-              src={selectedFile ? URL.createObjectURL(selectedFile) : userInfo.userImg}
+              src={selectedFile ? URL.createObjectURL(selectedFile) : userInfoData.userImg}
+              name="userImg"
               alt="Selected Photo"
               style={{ cursor: 'pointer' }}
               onClick={handleImageClick}
             />
             </div>
             <div className="name-email">
-              <div className="user-name">{userInfo.name}</div>
-              <div className="user-email">{userInfo.email}</div>
+              <div className="user-name">{userInfoData.name}</div>
+              <div className="user-email">{userInfoData.email}</div>
             </div>
           </div>
         </div>
 
         
         <div className="info-col">
-        <form  onSubmit={handleUpload}>
-          <label className="info-title">Name</label>
-          <input 
-              type="text"
-              name="name" 
-              placeholder={userInfo.name} 
-              onChange={handleInputChange} 
-              required
-          />
-          <div className="info-title">Notification</div>
-          <div className="sent-email-margin">
-          <input 
-              type="checkbox"
-              name="notification" 
-              checked={isSentEmail}
-              onChange={handleCheckboxChange}
-          />
-          <div className="sent-email">Email</div>
-          </div>
+          
+            <label className="info-title">Name</label>
+            <input 
+                type="text"
+                name="name" 
+                placeholder={userInfoData.name} 
+                value={formData.name}
+                onChange={handleInputChange} 
+                required
+            />
+            <div className="info-title">Notification</div>
+            <div className="sent-email-margin">
+            <input 
+                type="checkbox"
+                name="notification" 
+                checked={isSentEmail}
+                onChange={handleCheckboxChange}
+            />
+            <div className="sent-email">Email</div>
+            </div>
+            
+
+            <label className="info-title">Current Password</label>
+            <input 
+                type="password"
+                name="oldpassword" 
+                placeholder="Current Password" 
+                value={formData.oldpassword}
+                required
+                onChange={handleInputChange} 
+            />
+
+            <label className="info-title">New Password</label>
+            <input 
+                type="password"
+                name="newpassword" 
+                placeholder="New Password" 
+                value={formData.newpassword}
+                required
+                onChange={handleInputChange} 
+            />  
+
+
+            <label className="info-title">New Password (again)</label>
+            <input 
+                type="password"
+                name="newpassword2" 
+                placeholder="New Password (again)" 
+                value={formData.newpassword2}
+                required
+                onChange={handleInputChange} 
+            />  
+            <div className="sent-edit">
+                <button type="submit" onClick={sentEditInfo} className="button"> Finished </button>                    
+            </div>
+          
           
 
-          <label className="info-title">Current Password</label>
-          <input 
-              type="text"
-              name="oldpassword" 
-              placeholder="Current Password" 
-              required
-              onChange={handleInputChange} 
-          />
-
-          <label className="info-title">New Password</label>
-          <input 
-              type="text"
-              name="newpassword" 
-              placeholder="New Password" 
-              required
-              onChange={handleInputChange} 
-          />  
-
-
-          <label className="info-title">New Password (again)</label>
-          <input 
-              type="text"
-              name="newpassword2" 
-              placeholder="New Password (again)" 
-              required
-              onChange={handleInputChange} 
-          />  
-          <div className="sent-edit">
-              <button type="submit" onClick={sentEditInfo} className="button"> Finished </button>                    
           </div>
         </form>
-          
-
-        </div>
       </div>
 
 

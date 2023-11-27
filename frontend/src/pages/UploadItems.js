2d81c68e02fccedd18b4c255d78a6b3131ff13d1
林友文 <wendell@linyouwendeMacBook-Air.local>
2023-11-27 23:22:42 +0800

    Try my best to set the CSS of item and its detail

--- frontend/src/pages/UploadItems.js
+++ frontend/src/pages/UploadItems.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './UploadItems.css'
 import itemJson from '../components/Item.json'
+import { useNavigate } from 'react-router-dom';
 
 const UploadItems = () => {
   const [formData, setFormData] = useState(itemJson);
+  const [succeedMsg, setSucceedMsg] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     document.body.style.backgroundColor = "#91968a"; // Set your desired color
@@ -56,12 +59,27 @@ const UploadItems = () => {
       </label>
     </div>
   );
+  const handleUpload = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setSucceedMsg("Sucessfully upload item");
+  }
 
   return (
     <>
+    {succeedMsg && (
+      <div className="modal-backdrop">
+        <div className="modal">
+          <h2>{succeedMsg}</h2>
+          <button 
+            className='close-button'
+            onClick={() => setTimeout(() => {navigate('/viewitems')}, 100)}>Close</button>
+        </div>
+      </div>
+    )}
       <div className='parent-container'>
         <div className='upload-container'>
-          <form onSubmit={(e) => e.preventDefault()} style={{ display: 'flex', flexDirection: 'column', alignItems: 'start', gap: '5px' }}>
+          <form onSubmit={handleUpload} style={{ display: 'flex', flexDirection: 'column', alignItems: 'start', gap: '5px' }}>
             <input
               type="file"
               multiple
@@ -73,7 +91,7 @@ const UploadItems = () => {
             {renderImageScroll()}
             {/* <button type="button" onClick={() => document.getElementById('image-upload').click()}>Select Images</button> */}
             <label>Name</label>
-            <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
+            <input type="text" name="name" value={formData.name} onChange={handleInputChange} required/>
             <label>Category</label>
             <select name="category" value={formData.category} onChange={handleInputChange}>
               <option value="Snack">Snack</option>
@@ -81,15 +99,22 @@ const UploadItems = () => {
               {/* Add your categories here */}
             </select>
             <label>Amount</label>
-            <input type="number" name="amount" value={formData.amount} onChange={handleInputChange} />
+            <input 
+              type="number" 
+              name="amount" 
+              min={1}
+              value={formData.amount} 
+              onChange={handleInputChange}
+              required
+            />
             <label>Expiration Time</label>
-            <input type="time" name="expirationTime" value={formData.expirationTime} onChange={handleInputChange} />
+            <input type="datetime-local" name="expirationTime" value={formData.expirationTime} onChange={handleInputChange} required/>
             <label>Location</label>
-            <input type="text" name="location" value={formData.location} onChange={handleInputChange} />
+            <input type="text" name="location" value={formData.location} onChange={handleInputChange} required/>
             <label>Description</label>
             <textarea name="description" value={formData.description} onChange={handleInputChange} />
+            <button className='upload-button' type="submit">Upload</button>
           </form>
-          <button className='upload-button' type="submit">Upload</button>
         </div>
       </div>
     </>

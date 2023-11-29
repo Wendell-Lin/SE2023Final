31f4f01d81e8f75b7e3a5e5217bbd80fd914a7b9
Oscar Peng <oscar5206@gmail.com>
2023-11-29 22:39:10 +0800

    chore: remove import

--- backend/src/main/java/com/feastforward/model/Item.java
+++ backend/src/main/java/com/feastforward/model/Item.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import org.checkerframework.checker.units.qual.N;
 import org.hibernate.annotations.BatchSize;
 import org.springframework.format.annotation.DateTimeFormat;
 

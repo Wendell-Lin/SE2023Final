08b0a51efa160f819f8686ff9b69e571a52ac938
Techuang <chuangellow@gmail.com>
2023-11-30 23:07:48 +0800

    Add conditional navigation to view and upload items based on user authentication

--- frontend/src/components/Customlink.js
+++ frontend/src/components/Customlink.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
-function CustomLink({ to, children, ...props }) {
+function CustomLink({ to, children, onLinkClick, ...props }) {
     const resolvedPath = useResolvedPath(to);
 
     const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
+    const handleClick = (e) => {
+        if (onLinkClick) {
+            onLinkClick(e);
+        }
+    };
+
     return (
         <li className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>
+            <Link to={to} onClick={handleClick} {...props}>
                 {children}
             </Link>
         </li>

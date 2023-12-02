a45d5d69e44805bacef2c1d938662e6e89f6bb60
Oscar Peng <oscar5206@gmail.com>
2023-12-02 10:02:29 +0800

    fix: security config for /api/user

--- backend/src/main/java/com/feastforward/config/WebSecurityConfig.java
+++ backend/src/main/java/com/feastforward/config/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
                         auth.requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/api/test/**").permitAll()
                                 .requestMatchers("/api/items/**").permitAll()
-                                .requestMatchers("/api/users/**").permitAll()
+                                .requestMatchers("/api/user/**").permitAll()
                                 .requestMatchers("/api/file/**").permitAll()
                                 .anyRequest().authenticated()
                 );

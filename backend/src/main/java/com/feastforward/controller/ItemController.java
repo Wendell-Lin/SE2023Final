cd1acfccc256312b2af133d47815e4fc05308327
Oscar Peng <oscar5206@gmail.com>
2023-12-04 05:11:18 +0800

    fix: handle type conversion

--- backend/src/main/java/com/feastforward/controller/ItemController.java
+++ backend/src/main/java/com/feastforward/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.feastforward.controller;
 
 import com.feastforward.model.Item;
+import com.feastforward.model.User;
 import com.feastforward.model.dto.ItemDto;
 import com.feastforward.model.dto.Mapper;
 import com.feastforward.payload.request.CreateItemRequest;
@@ -8,12 +9,20 @@ import com.feastforward.payload.request.UpdateItemRequest;
 import com.feastforward.payload.response.GenericResponse;
 import com.feastforward.repository.ItemRepository;
 import com.feastforward.service.ItemService;
+import com.google.api.gax.rpc.UnauthenticatedException;
+
+import aj.org.objectweb.asm.Type;
+import autovalue.shaded.kotlinx.metadata.internal.metadata.jvm.deserialization.JvmMemberSignature.Method;
+
+import org.hibernate.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.Optional;
 import java.util.Date;
@@ -30,7 +39,7 @@ public class ItemController {
     ItemRepository itemRepository;
 
     @Autowired
-    ItemService ItemService;
+    ItemService itemService;
 
     @Autowired
     Mapper mapper;
@@ -46,10 +55,15 @@ public class ItemController {
     @PostMapping("/createItem")
     public ResponseEntity<?> createItem(@RequestBody CreateItemRequest itemRequest) {
         try {
-            Item _item = ItemService.createItem(itemRequest);
+            Item _item = itemService.createItem(itemRequest);
             ItemDto _itemDto = mapper.mapItemToItemDto(_item);
             return new ResponseEntity<>(_itemDto, HttpStatus.CREATED);
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
+            // check if e message is user not authenticated
+            if (e.getMessage().equals("User is not authenticated")) {
+                return new ResponseEntity<>(new GenericResponse("User is not authenticated"), HttpStatus.UNAUTHORIZED);
+            }
             throw new RuntimeException("An error occurred while creating the item: " + e.getMessage());
         }
     }
@@ -72,43 +86,56 @@ public class ItemController {
     }
 
     @PutMapping("/updateItem/{itemId}")
-    public ResponseEntity<?> updateItem(@PathVariable(value = "itemId") Long itemId, @RequestBody UpdateItemRequest itemRequest) {
+    public ResponseEntity<?> updateItem(@PathVariable(value = "itemId") String itemId, @RequestBody UpdateItemRequest itemRequest) {
         try {
-            Optional<Item> item = itemRepository.findById(itemId);
+            // Convert String to Long
+            Long itemIdLong = Long.parseLong(itemId);
+            Optional<Item> item = itemRepository.findById(itemIdLong);
             if (item.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
-            Item _item = ItemService.updateItem(itemId, itemRequest);
+            Item _item = itemService.updateItem(itemIdLong, itemRequest);
             ItemDto _itemDto = mapper.mapItemToItemDto(_item);
             return new ResponseEntity<>(_itemDto, HttpStatus.OK);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Error: Item ID is not a number.");
         } catch (Exception e) {
             throw new RuntimeException("An error occurred while updating the item: " + e.getMessage());
         }
     }
 
     @GetMapping("/getItemDetail/{itemId}")
-    public ResponseEntity<?> getItemDetail(@PathVariable(value = "itemId") Long itemId) {
+    public ResponseEntity<?> getItemDetail(@PathVariable(value = "itemId") String itemId) {
         try {
-            Optional<Item> item = itemRepository.findById(itemId);
+            // Convert String to Long
+            Long itemIdLong = Long.parseLong(itemId);
+            Optional<Item> item = itemRepository.findById(itemIdLong);
             if (item.isEmpty()) {
-                throw new RuntimeException("Error: Item is not found.");
+                // throw new RuntimeException("Error: Item is not found.");
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
             ItemDto itemDto = mapper.mapItemToItemDto(item.get());
             return new ResponseEntity<>(itemDto, HttpStatus.OK);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Error: Item ID is not a number.");
         } catch (Exception e) {
             throw new RuntimeException("An error occurred while getting the item detail: " + e.getMessage());
         }
     }
 
     @DeleteMapping("/deleteItem/{itemId}")
-    public ResponseEntity<?> deleteItem(@PathVariable(value = "itemId") Long itemId) {
+    public ResponseEntity<?> deleteItem(@PathVariable(value = "itemId") String itemId) {
         try {
-            Optional<Item> item = itemRepository.findById(itemId);
+            // Convert String to Long
+            Long itemIdLong = Long.parseLong(itemId);
+            Optional<Item> item = itemRepository.findById(itemIdLong);
             if (item.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
-            itemRepository.deleteById(itemId);
+            itemService.deleteItem(item.get());
             return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Error: Item ID is not a number.");
         } catch (Exception e) {
             throw new RuntimeException("An error occurred while deleting the item: " + e.getMessage());
         }

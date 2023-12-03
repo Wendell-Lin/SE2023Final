193f04ab8eb27fa57ee66137e2c48f65167920ed
Wendell-Lin <s950085az@gmail.com>
2023-12-04 00:01:59 +0800

    Test map, but marker are not testable

--- frontend/cypress/e2e/SearchItems.cy.js
+++ frontend/cypress/e2e/SearchItems.cy.js
@@ -18,6 +18,20 @@ describe('Search Component', () => {
         cy.visit('http://localhost:3000/viewitems');
         cy.wait('@getItems');
     });
+    it('filters no items based on search term', () => {
+        const searchTerm = 'Electronics'; // Replace with a relevant term from your FakeData
+        cy.get('.search-bar input[type="text"]').type(searchTerm);
+
+        cy.fixture('FakeData').then((data) => {
+            const filteredItems = data.uploadedItems.filter(item =>
+                item.name.includes(searchTerm) || item.description.includes(searchTerm)
+            );
+            cy.get('.list-container').children().should('have.length', filteredItems.length);
+            filteredItems.forEach(filteredItem => {
+                cy.get('.list-container').should('contain', filteredItem.name);
+            });
+        });
+    });
     it('filters existing items based on search term', () => {
         cy.fixture('FakeData').then((data) => {
             // Assuming that data.uploadedItems[0] exists and has a 'name' property
@@ -31,20 +45,6 @@ describe('Search Component', () => {
 
             cy.get('.list-container').children().should('have.length', filteredItems.length);
             cy.get('.list-container').should('contain', searchTerm);
-        });
-    });
-    it('filters no items based on search term', () => {
-        const searchTerm = 'Electronics'; // Replace with a relevant term from your FakeData
-        cy.get('.search-bar input[type="text"]').type(searchTerm);
-
-        cy.fixture('FakeData').then((data) => {
-            const filteredItems = data.uploadedItems.filter(item =>
-                item.name.includes(searchTerm) || item.description.includes(searchTerm)
-            );
-            cy.get('.list-container').children().should('have.length', filteredItems.length);
-            filteredItems.forEach(filteredItem => {
-                cy.get('.list-container').should('contain', filteredItem.name);
-            });
         });
     });
 });
